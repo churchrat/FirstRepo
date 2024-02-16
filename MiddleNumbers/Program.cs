a3a3eebf47112b23c575e1f5eda7978e5e695af6
churchrat <mihailkrustev08@gmail.com>
2024-02-16 12:31:16 +0200

    second commit

---
+++ MiddleNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MiddleNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int middle = number.Length / 2;
+            if (number.Length == 1)
+            {
+                Console.WriteLine(number[0]);
+            }
+            else if (number.Length % 2 == 0)
+            {
+                Console.WriteLine(number[middle - 1]);
+                Console.WriteLine(number[middle]);
+            }
+            else
+            {
+                Console.WriteLine(number[middle]);
+                Console.WriteLine(number[middle + 1]);
+                Console.WriteLine(number[middle + 2]);
+            }
+        }
+    }
+}
